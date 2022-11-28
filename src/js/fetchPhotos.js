aa39dc5c3db2f3a2d3b6768ec6c734a5cf5cfac2
yuriiklimenko <109259174+yuriiklimenko@users.noreply.github.com>
2022-11-28 11:50:29 +0200

    version 1.1.2

--- src/js/fetchPhotos.js
+++ src/js/fetchPhotos.js
@@ -1,3 +1,26 @@
+// const axios = require('axios').default;
+
+// export async function fetchPhotos(value, page) {
+//   const BASE_URL = 'https://pixabay.com/api';
+
+//   // axios приймає другим параметром об'єкт у якого властивість(params) зі значенням об'єкт {}
+//   //  у якого властивість це значення це параметр( строки запиту
+//   const options = {
+//     params: {
+//       key: '30733564-741f3ed68786028815fba657d',
+//       image_type: 'photo',
+//       q: value,
+//       orientation: 'horizontal',
+//       safesearch: 'true',
+//       per_page: '40',
+//       page: page,
+//     },
+//   };
+
+//   return await axios.get(BASE_URL, options);
+// }
+// ====================================================
+// `${BASE_LINK}?key=${KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`;
 const axios = require('axios').default;
 
 export async function fetchPhotos(value, page) {
@@ -17,5 +40,7 @@ export async function fetchPhotos(value, page) {
     },
   };
 
-  return await axios.get(BASE_URL, options);
+  const response = await axios.get(BASE_URL, options);
+
+  return response;
 }
