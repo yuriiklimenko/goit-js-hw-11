79d01241aa2e66d9db3b113d7c9427bf99ed0d42
yuriiklimenko <109259174+yuriiklimenko@users.noreply.github.com>
2022-11-28 12:02:12 +0200

    version 1.1.2

--- src/js/fetchPhotos.js
+++ src/js/fetchPhotos.js
@@ -20,27 +20,14 @@
 //   return await axios.get(BASE_URL, options);
 // }
 // ====================================================
-// `${BASE_LINK}?key=${KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`;
 const axios = require('axios').default;
 
-export async function fetchPhotos(value, page) {
-  const BASE_URL = 'https://pixabay.com/api';
+export async function fetchPhotos(searchQuery, page) {
+  const BASE_LINK = 'https://pixabay.com/api';
 
-  // axios приймає другим параметром об'єкт у якого властивість(params) зі значенням об'єкт {}
-  //  у якого властивість це значення це параметр( строки запиту
-  const options = {
-    params: {
-      key: '30733564-741f3ed68786028815fba657d',
-      image_type: 'photo',
-      q: value,
-      orientation: 'horizontal',
-      safesearch: 'true',
-      per_page: '40',
-      page: page,
-    },
-  };
+  const KEY = '30733564-741f3ed68786028815fba657d';
 
-  const response = await axios.get(BASE_URL, options);
-
-  return response;
+  return await axios.get(
+    `${BASE_LINK}?key=${KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
+  );
 }
